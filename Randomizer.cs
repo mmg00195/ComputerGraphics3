2792185a86c1d9725ef54d33c620cdb6e36b0c73
Manuel Molina García <mmg00195@red.ujaen.es>
2024-11-22 19:43:36 +0200

    Project Restart using .NET core

---
+++ Randomizer.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+using OpenTK.Mathematics;
+
+namespace ComputerGraphics3
+{
+    internal class Randomizer
+    {
+        private Random r;
+        private const int LOW_INT_VAL = -25;
+        private const int HIGH_INT_VAL = 25;
+        private const int LOW_COORD_VAL = 50;
+        private const int HIGH_COORD_VAL = 50;
+
+        public Randomizer()
+        {
+            r = new Random();
+        }
+
+        public Color RandomColor()
+        {
+            int genR = r.Next(0, 255);
+            int genG = r.Next(0, 255);
+            int genB = r.Next(0, 255);
+
+            Color col = Color.FromArgb(genR, genG, genB);
+
+            return col;
+        }
+
+        public Vector3 Random3DPoint()
+        {
+            int genA = r.Next(LOW_COORD_VAL, HIGH_COORD_VAL);
+            int genB = r.Next(LOW_COORD_VAL, HIGH_COORD_VAL);
+            int genC = r.Next(LOW_COORD_VAL, HIGH_COORD_VAL);
+            Vector3 vec = new Vector3(genA, genB, genC);
+            return vec;
+        }
+
+
+        public int RandomInt()
+        {
+            int i = r.Next(LOW_INT_VAL, HIGH_INT_VAL);
+            return i;
+        }
+
+        public int RandomInt(int MIN_VAL, int MAX_VAL)
+        {
+            int i = r.Next(MIN_VAL, MAX_VAL);
+            return i;
+        }
+
+        public int RandomInt(int MAX_VAL)
+        {
+            int i = r.Next(MAX_VAL);
+            return i;
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics3
+{
+    internal class Room
+    {
+        private readonly float[] _vertices =
+        {
+            // Position
+            -0.5f, -0.5f, -0.5f, // Front face
+            0.5f, -0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+
+            -0.5f, -0.5f,  0.5f, // Back face
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
+
+            -0.5f,  0.5f,  0.5f, // Left face
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+
+            0.5f,  0.5f,  0.5f, // Right face
+            0.5f,  0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+
+            -0.5f, -0.5f, -0.5f, // Bottom face
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f, -0.5f,
+
+            -0.5f,  0.5f, -0.5f, // Top face
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f, -0.5f
+        };
+    }
+}
